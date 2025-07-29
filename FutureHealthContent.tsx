0362ec0a358086fe58a83dae90306ec8f6329c0a
Dinesh <dineshpolavarapu66@gmail.com>
2025-07-29 11:03:07 +0530

    Make fixes

---
+++ FutureHealthContent.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+const FutureHealthContent: React.FC = () => {
+  return (
+    <div>
+      <h2>Future Health Predictions</h2>
+      <p>This is a placeholder for future health prediction content.</p>
+    </div>
+  )
+}
+
+export default FutureHealthContent
